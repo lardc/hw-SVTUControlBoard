eaf15fddc25358841dc5aa130c70d5b71a32d92d
asytyi <alexander.a.sytyi@gmail.com>
2020-07-27 18:20:22 +0300

    Внесены новые функции в заголовочный файл LowLevel.h

--- Firmware/Source/Controller/LowLevel.h
+++ Firmware/Source/Controller/LowLevel.h
@@ -2,18 +2,16 @@
 #define __LOWLEVEL_H
 
 // Include
-#include "ZwGPIO.h"
-#include "ZwSPI.h"
+#include "stdinc.h"
 
 // Functions
 //
 void LL_ToggleBoardLED();
-//
-void LL_IFB_EN(bool State);
-void LL_RangeSelect(bool State);
-void LL_SYNC_SCPC(bool State);
-void LL_GATE_EN(bool State);
-void LL_SYNC_OSC(bool State);
+void LL_IgFeedback(bool State);
+void LL_IdLowRange(bool State);
+void LL_SyncPowerCell(bool State);
+void LL_IgPulse(bool State);
+void LL_SyncScope(bool State);
 void LL_WriteDAC(uint16_t Data);
 
-#endif //__LOWLEVEL_H
+#endif // __LOWLEVEL_H
