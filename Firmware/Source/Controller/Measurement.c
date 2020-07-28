cb30d56896ce830bdf78f7de689e5808f798a077
asytyi <alexander.a.sytyi@gmail.com>
2020-07-28 11:37:55 +0300

    Исправление форматирования Measurement.c

--- Firmware/Source/Controller/Measurement.c
+++ Firmware/Source/Controller/Measurement.c
@@ -19,11 +19,11 @@ void MEASURE_ConvertADCtoValx(uint16_t *InputArray, float *OutputArray, uint16_t
 {
 	float Offset = (float)((int16_t)DataTable[RegisterOffset]);
 	float K = (float)DataTable[RegisterK] / 1000;
-
+	
 	float P0 = (float)((int16_t)DataTable[RegisterP0]);
 	float P1 = (float)DataTable[RegisterP1] / 1000;
 	float P2 = (float)((int16_t)DataTable[RegisterP2]) / 1e6;
-
+	
 	for(uint16_t i = 0; i < DataLength; i++)
 	{
 		float tmp = ((float)InputArray[i] + Offset) * ADC_REF_VOLTAGE / ADC_RESOLUTION * K;
@@ -31,34 +31,39 @@ void MEASURE_ConvertADCtoValx(uint16_t *InputArray, float *OutputArray, uint16_t
 		OutputArray[i] = (tmp > 0) ? tmp : 0;
 	}
 }
-//------------------------------------------------------------------------------
+//------------------------------------
 
 void MEASURE_ConvertVd(uint16_t *InputArray, float *OutputArray, uint16_t DataLength)
 {
-	MEASURE_ConvertADCtoValx(InputArray, OutputArray, DataLength, REG_VD_OFFSET, REG_VD_K, REG_VD_P0, REG_VD_P1, REG_VD_P2);
+	MEASURE_ConvertADCtoValx(InputArray, OutputArray, DataLength, REG_VD_OFFSET, REG_VD_K, REG_VD_P0, REG_VD_P1,
+			REG_VD_P2);
 }
 //------------------------------------
 
 void MEASURE_ConvertId(uint16_t *InputArray, float *OutputArray, uint16_t DataLength)
 {
-	MEASURE_ConvertADCtoValx(InputArray, OutputArray, DataLength, REG_ID_OFFSET, REG_ID_K, REG_ID_P0, REG_ID_P1, REG_ID_P2);
+	MEASURE_ConvertADCtoValx(InputArray, OutputArray, DataLength, REG_ID_OFFSET, REG_ID_K, REG_ID_P0, REG_ID_P1,
+			REG_ID_P2);
 }
 //------------------------------------
 
 void MEASURE_ConvertIdLow(uint16_t *InputArray, float *OutputArray, uint16_t DataLength)
 {
-	MEASURE_ConvertADCtoValx(InputArray, OutputArray, DataLength, REG_IDL_OFFSET, REG_IDL_K, REG_IDL_P0, REG_IDL_P1, REG_IDL_P2);
+	MEASURE_ConvertADCtoValx(InputArray, OutputArray, DataLength, REG_IDL_OFFSET, REG_IDL_K, REG_IDL_P0, REG_IDL_P1,
+			REG_IDL_P2);
 }
 //------------------------------------
 
 void MEASURE_ConvertVg(uint16_t *InputArray, float *OutputArray, uint16_t DataLength)
 {
-	MEASURE_ConvertADCtoValx(InputArray, OutputArray, DataLength, REG_VG_OFFSET, REG_VG_K, REG_VG_P0, REG_VG_P1, REG_VG_P2);
+	MEASURE_ConvertADCtoValx(InputArray, OutputArray, DataLength, REG_VG_OFFSET, REG_VG_K, REG_VG_P0, REG_VG_P1,
+			REG_VG_P2);
 }
 //------------------------------------
 
 void MEASURE_ConvertIg(uint16_t *InputArray, float *OutputArray, uint16_t DataLength)
 {
-	MEASURE_ConvertADCtoValx(InputArray, OutputArray, DataLength, REG_IG_OFFSET, REG_IG_K, REG_IG_P0, REG_IG_P1, REG_IG_P2);
+	MEASURE_ConvertADCtoValx(InputArray, OutputArray, DataLength, REG_IG_OFFSET, REG_IG_K, REG_IG_P0, REG_IG_P1,
+			REG_IG_P2);
 }
 //------------------------------------
