9737f42d310fcec9b235d4d6f60fe73e459a2c59
IvanovDmtr <d.ivanov@protonelectrotex.com>
2021-01-18 14:26:18 +0300

    Исправлен Constraints.c для регистра 33

--- Firmware/Source/Platform/Constraints.c
+++ Firmware/Source/Platform/Constraints.c
@@ -36,7 +36,7 @@ const TableItemConstraint NVConstraint[DATA_TABLE_NV_SIZE] = {
 		{0, 0, 0},																// 30
 		{0, 0, 0},																// 31
 		{0, INT16U_MAX, 0},														// 32
-		{0, 0, 0},																// 33
+		{0, INT16U_MAX, 0},														// 33
 		{0, 0, 0},																// 34
 		{0, 0, 0},																// 35
 		{0, 0, 0},																// 36
