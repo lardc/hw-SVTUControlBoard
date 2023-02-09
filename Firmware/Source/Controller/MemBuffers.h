01e92119b6615bac5777605f71f0eab215e61827
d.ivanov <d.ivanov@proton-electrotex.com>
2023-02-09 15:01:46 +0300

    Добавлен эндпоинт тока затвора

--- Firmware/Source/Controller/MemBuffers.h
+++ Firmware/Source/Controller/MemBuffers.h
@@ -10,6 +10,7 @@ extern float MEMBUF_EP_Vd[VALUES_x_SIZE];
 extern float MEMBUF_EP_Id[VALUES_x_SIZE];
 extern float MEMBUF_EP_Vg[VALUES_x_SIZE];
 extern float MEMBUF_EP_VgErr[VALUES_x_SIZE];
+extern float MEMBUF_EP_Ig[VALUES_x_SIZE];
 
 extern volatile Int16U MEMBUF_DMA_Vd[VALUES_POWER_DMA_SIZE];
 extern volatile Int16U MEMBUF_DMA_Id[VALUES_POWER_DMA_SIZE];
