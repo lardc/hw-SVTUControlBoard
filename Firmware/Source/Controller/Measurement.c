e944ded22f508a31d67493441d41910cd66184b3
Alexander-Mamasaklisov <a.mamasakhlisov@proton-electrotex.com>
2025-02-28 18:18:02 +0300

    реализован пересчет из аналоговых значений в цифровые для второго канала ацп UT

--- Firmware/Source/Controller/Measurement.c
+++ Firmware/Source/Controller/Measurement.c
@@ -54,12 +54,12 @@ void MEASURE_ConvertVd(pFloat32 InputArray, Int16U DataLength, bool CheckChannel
 	if (CheckChannel)
 	{
 		MEASURE_ConvertADCtoValx(InputArray, DataLength, REG_VD_B, REG_VD_K, REG_VD_P0, REG_VD_P1,
-					REG_VD_P2, 0))
+					REG_VD_P2, 0);
 	}
 	else
 	{
-		MEASURE_ConvertADCtoValx(InputArray, DataLength, REG_VD_B, REG_VD_K, REG_VD_P0, REG_VD_P1,
-					REG_VD_P2, 0);
+		MEASURE_ConvertADCtoValx(InputArray, DataLength, REG_UTCH2_B, REG_UTCH2_K, REG_UTCH2_P0, REG_UTCH2_P1,
+				REG_UTCH2_P2, 0);
 	}
 }
 //------------------------------------
--- Firmware/Source/Controller/Measurement.h
+++ Firmware/Source/Controller/Measurement.h
@@ -5,7 +5,7 @@
 
 // Functions
 //
-void MEASURE_ConvertVd(pFloat32 InputArray, Int16U DataLength);
+void MEASURE_ConvertVd(pFloat32 InputArray, Int16U DataLength, bool CheckChannel);
 void MEASURE_ConvertId(pFloat32 InputArray, Int16U DataLength, Int16U CurrentRange);
 //
 float MEASURE_CollectorAverageCurrent();
