b22528c3e28eba4dc4a136a4d31f51f249d35370
Alexander-Mamasaklisov <a.mamasakhlisov@proton-electrotex.com>
2026-04-07 15:19:41 +0300

    Начальный индекс для расчета среднего сделан знаковым

--- Firmware/Source/Controller/Measurement.c
+++ Firmware/Source/Controller/Measurement.c
@@ -155,7 +155,7 @@ float MEASURE_CollectorAverageValue(pFloat32 MEMBUF_DMA_Intermediary)
 	int32_t MearusingPoints = (int32_t)DataTable[REG_MSR_TIME] * TIMER2_uS / TIMER1_uS;
 	int32_t Coef = (DataTable[REG_PULSE_DURATION] <= 5) ? (7 - (int32_t)DataTable[REG_PULSE_DURATION]) : 1;
 	int32_t Offset = (int32_t)DataTable[REG_UG_EDGE_TIME] / ( TIMER2_uS / TIMER1_uS) + Coef * (int32_t)DataTable[REG_PULSE_DURATION] * 1000 / TIMER2_uS;
-	Int16U StartIndex = Offset - MearusingPoints;
+	int32_t StartIndex = Offset - MearusingPoints;
 
 	if (StartIndex < 0)
 		StartIndex = 0;
